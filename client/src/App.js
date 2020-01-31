3f14a0ea1ba2524b3891bd492ef51f62d88b9b36
Tim Vo <timtvo714@gmail.com>
2020-01-30 19:50:55 -0800

    added dashboard fx

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,10 @@ import Landing from './components/layout/Landing';
 import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/profile-forms/CreateProfile';
+import PrivateRoute from './components/routing/PrivateRoute';
+
 //redux
 import {Provider} from 'react-redux';
 import store from './store';
@@ -32,6 +36,12 @@ const App = () => {
             <Switch>
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
+              <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
--- client/src/reducers/alert.js
+++ client/src/reducers/alert.js
@@ -1,21 +1,15 @@
-import {SET_ALERT, REMOVE_ALERT} from '../actions/types'
+import {SET_ALERT, REMOVE_ALERT} from '../actions/types';
 
-const initialState = [
-    {
-        id: 1,
-        msg: 'Please log in',
-        alertType: 'Success'
-    }
-]
+const initialState = [];
 
-export default function(state=initialState, action) {
-    const {type, payload} = action;
-    switch(action.type) {
-        case SET_ALERT:
-            return [...state, payload]
-        case REMOVE_ALERT: 
-            return state.filter(alert => alert.id !== payload);
-        default: 
-            return state;
-    }
+export default function (state = initialState, action) {
+  const {type, payload} = action;
+  switch (action.type) {
+    case SET_ALERT:
+      return [...state, payload];
+    case REMOVE_ALERT:
+      return state.filter (alert => alert.id !== payload);
+    default:
+      return state;
+  }
 }
