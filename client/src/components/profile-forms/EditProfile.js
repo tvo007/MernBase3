ad15cd108fbdbe359a703ca0be845f42a871bbce
Tim Vo <timtvo714@gmail.com>
2020-02-01 15:23:14 -0800

    finished dashboard

--- client/src/components/profile-forms/EditProfile.js
+++ client/src/components/profile-forms/EditProfile.js
@@ -1,10 +1,15 @@
-import React, {useState, Fragment} from 'react';
+import React, {useState, Fragment, useEffect} from 'react';
 import {Link, withRouter} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {createProfile, getCurrentProfile} from '../../actions/profile';
 
-const CreateProfile = ({createProfile, history}) => {
+const EditProfile = ({
+  profile: {profile, loading},
+  createProfile,
+  getCurrentProfile,
+  history,
+}) => {
   const [formData, setFormData] = useState ({
     company: '',
     website: '',
@@ -21,6 +26,57 @@ const CreateProfile = ({createProfile, history}) => {
   });
 
   const [displaySocialInputs, toggleSocialInputs] = useState (false);
+  const edit = history.location.pathname.includes ('edit');
+  useEffect (
+    () => {
+      getCurrentProfile ();
+    },
+    [getCurrentProfile]
+  );
+
+  //     setFormData ({
+  //       company: loading || !profile.company ? '' : profile.company,
+  //       website: loading || !profile.website ? '' : profile.website,
+  //       location: loading || !profile.location ? '' : profile.location,
+  //       status: loading || !profile.status ? '' : profile.status,
+  //       skills: loading || !profile.skills ? '' : profile.skills.join (','),
+  //       githubusername: loading || !profile.githubusername
+  //         ? ''
+  //         : profile.githubusername,
+  //       bio: loading || !profile.bio ? '' : profile.bio,
+  //       twitter: loading || !profile.social ? '' : profile.social.twitter,
+  //       facebook: loading || !profile.social ? '' : profile.social.facebook,
+  //       linkedin: loading || !profile.social ? '' : profile.social.linkedin,
+  //       youtube: loading || !profile.social ? '' : profile.social.youtube,
+  //       instagram: loading || !profile.social ? '' : profile.social.instagram,
+  //     });
+  //   },
+  //   [loading, getCurrentProfile]
+  // );
+
+  useEffect (
+    () => {
+      if (edit) {
+        setFormData ({
+          company: loading || !profile.company ? '' : profile.company,
+          website: loading || !profile.website ? '' : profile.website,
+          location: loading || !profile.location ? '' : profile.location,
+          status: loading || !profile.status ? '' : profile.status,
+          skills: loading || !profile.skills ? '' : profile.skills.join (','),
+          githubusername: loading || !profile.githubusername
+            ? ''
+            : profile.githubusername,
+          bio: loading || !profile.bio ? '' : profile.bio,
+          twitter: loading || !profile.social ? '' : profile.social.twitter,
+          facebook: loading || !profile.social ? '' : profile.social.facebook,
+          linkedin: loading || !profile.social ? '' : profile.social.linkedin,
+          youtube: loading || !profile.social ? '' : profile.social.youtube,
+          instagram: loading || !profile.social ? '' : profile.social.instagram,
+        });
+      }
+    },
+    [loading, edit, profile]
+  );
 
   const {
     company,
@@ -42,7 +98,7 @@ const CreateProfile = ({createProfile, history}) => {
 
   const onSubmit = e => {
     e.preventDefault ();
-    createProfile (formData, history);
+    createProfile (formData, history, true);
   };
 
   return (
@@ -220,14 +276,16 @@ const CreateProfile = ({createProfile, history}) => {
   );
 };
 
-CreateProfile.propTypes = {
+EditProfile.propTypes = {
   createProfile: PropTypes.func.isRequired,
+  getCurrentProfile: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = state => ({
   profile: state.profile,
 });
 
-export default connect (mapStateToProps, {createProfile}) (
-  withRouter (CreateProfile)
+export default connect (mapStateToProps, {createProfile, getCurrentProfile}) (
+  withRouter (EditProfile)
 );
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,7 @@ const {check, validationResult} = require ('express-validator');
 
 const Profile = require ('../../models/Profile');
 const User = require ('../../models/User');
+const Post = require ('../../models/Post');
 
 //@route GET api/profile/me
 //desc: get current users profile
@@ -159,7 +160,9 @@ router.get ('/user/:user_id', async (req, res) => {
 
 router.delete ('/', auth, async (req, res) => {
   try {
-    //todo: remove users posts
+    //remove user posts
+    await Post.postManyu ({user: req.user.id});
+
     //remove profiles
     await Profile.findOneAndRemove ({user: req.user.id});
     //remove user
